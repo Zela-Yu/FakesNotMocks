dcf2e7eb99508a9495df587c787f2410bfee13ab
Zhiliang Yu <zhiliangyu@bbq.local>
2025-02-26 22:48:36 -0800

    add fake test for cash

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+class FakeExchange implements Exchange {
+    @Override
+    public float rate(String origin, String target) {
+        return 1.2f;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         // Unit Test for Cash class
+        Exchange exchange = new FakeExchange();
 
         // Test setup
-        Exchange exchange = new NYSE("someAccessKey");
+        // Exchange exchange = new NYSE("someAccessKey");
         Cash dollar = new Cash(exchange, 100);
         System.out.println("Dollar: " + dollar.toString());
 
